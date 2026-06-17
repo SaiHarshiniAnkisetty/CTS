10f567a6764ac3a540032c706158b4603cd35b08
Sai Harshini Ankisetty <harshiniankisetty@gmail.com>
2026-06-17 20:29:50 +0530

    Add files via upload

---
+++ Deep Skilling/Module 2/Exercise 7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FinancialForecasting
+{
+    class Program
+    {
+        // Recursive Method
+        static double PredictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years)
+        {
+            // Base Case
+            if (years == 0)
+                return currentValue;
+
+            // Recursive Call
+            return PredictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter current value: ");
+            double current =
+                Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter growth rate (%): ");
+            double rate =
+                Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.Write("Enter years: ");
+            int years =
+                Convert.ToInt32(Console.ReadLine());
+
+            double future =
+                PredictFutureValue(
+                    current,
+                    rate,
+                    years);
+
+            Console.WriteLine(
+                "\nPredicted Future Value = "
+                + future);
+
+            Console.ReadLine();
+        }
+    }
+}
