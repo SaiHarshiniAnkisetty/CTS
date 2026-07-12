3a61537dfa83cf4e90c3f8b3aab3ef8efc525605
Sai Harshini Ankisetty <harshiniankisetty@gmail.com>
2026-07-13 00:28:28 +0530

    Add files via upload

---
+++ Deep Skilling/Module 4/1. Moq-Handson/CustomerCommTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> mockMailSender;
+        private CustomerCommLib.CustomerComm customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockMailSender = new Mock<IMailSender>();
+
+            customerComm = new CustomerCommLib.CustomerComm(mockMailSender.Object);
+        }
+
+        [TestCase]
+        public void SendMail_ReturnsTrue()
+        {
+            mockMailSender
+                .Setup(x => x.SendMail(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                .Returns(true);
+
+            bool result = customerComm.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
